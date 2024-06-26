9f95d871c25bf459d20e8b8a4898d709f32ea69f
4EyeCodes <EyeCodes@users.noreply.github.com>
2024-06-26 21:17:59 +0800

    deploy

--- function.js
+++ function.js
@@ -9,11 +9,13 @@ if (light == true){
 
   document.getElementById('body').style.backgroundColor = 'rgb(20, 18, 24)';
   document.getElementById('nav').style.backgroundColor = 'rgb(20, 18, 24)';
+  document.getElementById('headerOne').style.color = 'rgb(239, 233, 250)';
   document.getElementById('themeBTN').style.backgroundColor = '#eeed11';
 
   for(i = 1; i <= 4; i++){
 
     document.getElementById("menu-"+i).style.color = 'rgb(239, 233, 250)';
+    document.getElementById("coreValues-"+i).style.color = 'rgb(239, 233, 250)';
 
   }
 
@@ -23,6 +25,7 @@ if (light == true){
 
   document.getElementById('body').style.backgroundColor = 'white';
   document.getElementById('nav').style.backgroundColor = 'white';
+  document.getElementById('headerOne').style.color = 'rgb(20, 18, 24)';
   document.getElementById('themeBTN').style.backgroundColor = 'rgb(20, 18, 24)';
 
   for(i = 1; i <= 4; i++){
