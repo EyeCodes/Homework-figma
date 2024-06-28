a418ed070ab8ae8b17e4207f43d18348403764b3
4EyeCodes <EyeCodes@users.noreply.github.com>
2024-06-28 22:15:35 +0800

    go

--- function.js
+++ function.js
@@ -1,48 +1,23 @@
 // theme light/dark vvv
 var light = true;
 
-document.getElementById("themeBTN").onclick = function() {themeChanger()};
+document.getElementById("orderServiceBtn").onclick = function() {orderSeriveForm()};
+document.getElementById("form-container").onclick = function() {orderSeriveFormClose()};
 
-function themeChanger(){
+function orderSeriveForm(){
 
-if (light == true){
-
-  document.getElementById('body').style.backgroundColor = 'rgb(20, 18, 24)';
-  document.getElementById('nav').style.backgroundColor = 'rgb(20, 18, 24)';
-  document.getElementById('headerOne').style.color = 'rgb(239, 233, 250)';
-  document.getElementById('themeBTN').style.backgroundColor = '#eeed11';
-
-  for(i = 1; i <= 4; i++){
-
-    document.getElementById("menu-"+i).style.color = 'rgb(239, 233, 250)';
-    document.getElementById("coreValues-"+i).style.color = 'rgb(239, 233, 250)';
-    document.getElementById("nameID-"+i).style.color = 'rgb(239, 233, 250)';
-    document.getElementById("featureID-"+i).style.backgroundColor = 'rgb(10, 0, 40)';
-
-  }
-
-  light = false;
-
-}else{
-
-  document.getElementById('body').style.backgroundColor = 'white';
-  document.getElementById('nav').style.backgroundColor = 'white';
-  document.getElementById('headerOne').style.color = 'rgb(20, 18, 24)';
-  document.getElementById('themeBTN').style.backgroundColor = 'rgb(20, 18, 24)';
-
-  for(i = 1; i <= 4; i++){
-
-    document.getElementById("menu-"+i).style.color = 'rgb(10, 0, 40)';
-    document.getElementById("coreValues-"+i).style.color = 'rgb(10, 0, 40)';
-    document.getElementById("nameID-"+i).style.color = 'rgb(10, 0, 40)';
-    document.getElementById("featureID-"+i).style.backgroundColor = 'rgb(239, 233, 250)';
-
-  }
-
-  light = true;
+  document.getElementById("form-container").setAttribute("class", "form-section");
+  document.getElementById("form-fill-up").setAttribute("class", "form-order-service");
 
 }
 
+function orderSeriveFormClose(){
+
+  document.getElementById("form-container").removeAttribute("class", "form-section");
+  document.getElementById("form-fill-up").removeAttribute("class", "form-order-service");
+
 }
+
+
 
 //_____________________________________________________________________________________________
