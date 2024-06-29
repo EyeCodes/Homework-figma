9659466c236105a0982ce1efda6971945116ff44
4EyeCodes <EyeCodes@users.noreply.github.com>
2024-06-29 23:44:28 +0800

    go

--- function.js
+++ function.js
@@ -2,17 +2,18 @@
 var light = true;
 
 document.getElementById("orderServiceBtn").onclick = function() {orderSeriveForm()};
-document.getElementById("form-container").onclick = function() {orderSeriveFormClose()};
+document.getElementById("closeBTN").onclick = function() {orderSeriveFormClose()};
 
 function orderSeriveForm(){
 
   document.getElementById("form-container").setAttribute("class", "form-section");
   document.getElementById("form-fill-up").setAttribute("class", "form-order-service");
-
+  document.getElementById("form-fill-up").removeAttribute("style", "display:none");
 }
 
 function orderSeriveFormClose(){
 
+  document.getElementById("form-fill-up").setAttribute("style", "display:none");
   document.getElementById("form-container").removeAttribute("class", "form-section");
   document.getElementById("form-fill-up").removeAttribute("class", "form-order-service");
 
