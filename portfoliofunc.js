be01eabb14e4a2c36f5a6b82987a58e0879ef096
4EyeCodes <EyeCodes@users.noreply.github.com>
2024-07-01 03:21:33 +0800

    go

--- portfoliofunc.js
+++ portfoliofunc.js
@@ -1,8 +1,4 @@
 
-// form
-document.getElementById("orderServiceBtn").onclick = function() {orderServiceForm()};
-document.getElementById("closeBTN").onclick = function() {orderServiceFormClose()};
-
 // Mobile Collapse
 document.getElementById("menu").onclick = function() {navMenuCollapse()};
 document.getElementById("closeMenu").onclick = function() {navMenuCollapseClose()};
@@ -14,21 +10,6 @@ window.onresize = function() {
   document.getElementById("closeMenu").setAttribute("style", "display:none");
 
 };
-
-function orderServiceForm(){
-
-  document.getElementById("form-container").setAttribute("class", "form-section");
-  document.getElementById("form-fill-up").setAttribute("class", "form-order-service");
-  document.getElementById("form-fill-up").removeAttribute("style", "display:none");
-}
-
-function orderServiceFormClose(){
-
-  document.getElementById("form-fill-up").setAttribute("style", "display:none");
-  document.getElementById("form-container").removeAttribute("class", "form-section");
-  document.getElementById("form-fill-up").removeAttribute("class", "form-order-service");
-
-}
 
 // menu collapse
 
@@ -49,5 +30,3 @@ function navMenuCollapseClose(){
 }, 100);
 
 }
-
-//_____________________________________________________________________________________________
